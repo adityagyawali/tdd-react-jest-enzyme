42b8043620e77e677708e8a71bc3e42adb8153f8
Aditya Gyawali <gyawali_aditya@hotmail.com>
2018-12-29 23:01:16 +0200

    test setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import Calculator from "./component/Calculator";
+// import Calculator from "./component/Calculator";
 import "./App.css";
+
+import ClickCounter from "./component/ClickCounter";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Calculator />
+				<ClickCounter />
 			</div>
 		);
 	}
--- src/App.spec.js
+++ src/App.spec.js
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import App from "./App";
-import Calculator from "./component/Calculator";
 
 describe("App component", () => {
 	let wrapper;
@@ -12,13 +11,5 @@ describe("App component", () => {
 
 	it("should render a div", () => {
 		expect(wrapper.find("div").length).toEqual(1);
-	});
-
-	it("should render a calculator component", () => {
-		expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
-	});
-
-	it("should render correctly", () => {
-		expect(wrapper).toMatchSnapshot();
 	});
 });
